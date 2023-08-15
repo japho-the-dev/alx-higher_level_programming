1121c55e48e6fcd411b4a63b10ecdce43d7ce5d4
JAPHETH <japho0740@gmail.com>
2023-08-15 07:42:09 +0000

    changes on the file 10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,20 +1,14 @@
-#include <Python.h>
-#include <object.h>
-#include <listobject.h>
+#!/usr/bin/python3
 
-/**
-* print_python_list_info -  outputs basic information about python list
-* @p: python object
-**/
 
-void print_python_list_info(PyObject *p)
-{
-	long int size = PyList_Size(p);
-	int i;
-	PyListObject *obj = (PyListObject *)p;
+def divisible_by_2(my_list=[]):
+    if not my_list:
+        return None
 
-	printf("[*] Size of the Python List = %li\n", size);
-	printf("[*] Allocated = %li\n", obj->allocated);
-	for (i = 0; i < size; i++)
-		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
-}
+    new_list = []
+    for num in my_list:
+        if num % 2 == 0:
+            new_list.append(True)
+        else:
+            new_list.append(False)
+    return new_list
