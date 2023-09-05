397a4cbe72075388239b4cc253f9792d68f9b3ec
JAPHETH <japho0740@gmail.com>
2023-09-05 19:34:27 +0000

    Updated solution to Task 0

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+"""A class that defines a rectangle"""
 
-"""Defines a Rectangle class."""
 
 class Rectangle:
-    """Represent a rectangle."""
+    """An empty class that represents a rectangle"""
     pass
